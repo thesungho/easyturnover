536bb01ceb1465b131f7071d78d9e12a3e8f7897
Sungho Yoo <sunghoster@gmail.com>
2013-11-26 17:49:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easyturnover::Application.config.secret_key_base = '0eedd3bcfbdcebad6e55516fd23e45a7c6f76db7f15f3535b43fdf6bbc9543de6e0ab6aa2fd348342b07530bf4142aa0acfe964f60afcfd24c15bbeb4cfe7219'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Easyturnover::Application.config.session_store :cookie_store, key: '_easyturnover_session'
